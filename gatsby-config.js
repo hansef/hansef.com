625463588c2970e8ffa21087d4416796646b9f0b
Hans Friedrich <hans@bletchley.co>
2020-09-25 11:12:07 -0700

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,17 +5,17 @@ const { theme } = resolveConfig(tailwindConfig)
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter Tailwind CSS",
-    author: "Melanie Nolan",
-    description: "A Gatsby starter to set you up with Tailwind CSS",
+    title: "Hans Friedrich",
+    author: "Hans Friedrich",
+    description: "Hans Friedrich | Portland, Oregon"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "Gatsby Starter Tailwind CSS",
-        short_name: "Gatsby Starter Tailwind CSS",
+        name: "Hans Friedrich",
+        short_name: "Hans Friedrich",
         start_url: "/",
         background_color: theme.colors.white,
         theme_color: theme.colors.teal[500],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Layout from "../components/Layout"
-import heroImage from "../images/hero-image.svg"
 
 export default () => {
   return (
@@ -9,34 +8,12 @@ export default () => {
         <div className="sm:flex sm:flex-row-reverse sm:items-center">
           <div className="sm:px-2">
             <h1 className="px-4 pt-5 text-2xl text-left text-teal-500 font-bold sm:text-3xl">
-              Gatsby Starter Tailwind CSS
+              inveterate generalist. managing director &amp; cto at bletchley.
             </h1>
-            <p className="px-4 mt-8 text-lg text-gray-700 sm:mt-8">
-              Start your Gatsby and Tailwind CSS project easily with this Gatsby
-              Starter
+            <p className="px-4 text-lg text-gray-700">
+              <a href="mailto:hans@bletchley.co">hans@bletchley.co</a>
             </p>
           </div>
-          <img
-            className="w-full max-w-lg mt-16 mx-auto sm:w-1/2"
-            src={heroImage}
-            alt="Gatsby and Tailwind CSS together"
-          />
-        </div>
-
-        <div className="mx-auto">
-          <a
-            href="https://github.com/melanienolan/gatsby-starter-tailwind-css"
-            className="p-4 bg-teal-500 text-white text-xs rounded shadow hover:bg-teal-400"
-          >
-            View on GitHub
-          </a>
-        </div>
-
-        <div className="flex items-center p-3 mx-2 bg-white rounded shadow-xs sm:mx-auto">
-          <code className="text-gray-600 text-xs leading-xl">
-            gatsby new my-project-name
-            https://github.com/melanienolan/gatsby-starter-tailwind-css
-          </code>
         </div>
       </main>
     </Layout>
