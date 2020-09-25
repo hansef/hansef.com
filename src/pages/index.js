9deeafdf6aacc71a135ab08739280677114b7294
Hans Friedrich <hans@bletchley.co>
2020-09-25 11:34:32 -0700

    Add email and twitter links

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,14 @@ export default () => {
       <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around">
         <div className="sm:flex sm:flex-row-reverse sm:items-center">
           <div className="sm:px-2">
-            <h1 className="px-4 pt-5 text-2xl text-left text-teal-500 font-bold sm:text-3xl">
+            <h1 className="px-4 py-5 text-2xl text-left text-teal-500 font-bold sm:text-3xl">
               inveterate generalist. managing director &amp; cto at bletchley.
             </h1>
-            <p className="px-4 text-lg text-gray-700">
-              <a href="mailto:hans@bletchley.co">hans@bletchley.co</a>
+            <p className="px-4 py-2 text-lg text-gray-700">
+              <a href="mailto:hans@bletchley.co" className="inline text-gray-600"><svg xmlns="http://www.w3.org/2000/svg" className="inline fill-current" width="24" height="24" viewBox="0 0 24 24"><path d="M12 12.713l-11.985-9.713h23.971l-11.986 9.713zm-5.425-1.822l-6.575-5.329v12.501l6.575-7.172zm10.85 0l6.575 7.172v-12.501l-6.575 5.329zm-1.557 1.261l-3.868 3.135-3.868-3.135-8.11 8.848h23.956l-8.11-8.848z"/></svg> <span className="pl-2">hans@bletchley.co</span></a>
+            </p>
+            <p className="px-4 py-2 text-lg text-gray-700">
+              <a href="https://twitter.com/hansef" className="inline text-gray-600"><svg className="inline fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-.139 9.237c.209 4.617-3.234 9.765-9.33 9.765-1.854 0-3.579-.543-5.032-1.475 1.742.205 3.48-.278 4.86-1.359-1.437-.027-2.649-.976-3.066-2.28.515.098 1.021.069 1.482-.056-1.579-.317-2.668-1.739-2.633-3.26.442.246.949.394 1.486.411-1.461-.977-1.875-2.907-1.016-4.383 1.619 1.986 4.038 3.293 6.766 3.43-.479-2.053 1.08-4.03 3.199-4.03.943 0 1.797.398 2.395 1.037.748-.147 1.451-.42 2.086-.796-.246.767-.766 1.41-1.443 1.816.664-.08 1.297-.256 1.885-.517-.439.656-.996 1.234-1.639 1.697z"/></svg> <span className="pl-2">@hansef</span></a>
             </p>
           </div>
         </div>
